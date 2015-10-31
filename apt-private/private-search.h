eba5e371c3b49e44771ce7b7ac1c8727d69fb418
Luiz Oliveira <ziuloliveira@gmail.com>
2015-10-31 18:11:14 -0200

    Included search using Levenshtein when result is empty
    Signed-off-by: Luiz Oliveira <ziuloliveira@gmail.com>

--- apt-private/private-search.h
+++ apt-private/private-search.h
@@ -8,5 +8,6 @@ class CommandLine;
 APT_PUBLIC bool FullTextSearch(CommandLine &CmdL);
 bool identify_regex(std::vector<std::string> input);
 int RabinKarp(std::string StringInput, std::string Pattern);
+int levenshtein_distance(const std::string &s1, const std::string &s2);
 
 #endif
